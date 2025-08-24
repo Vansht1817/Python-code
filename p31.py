a09a6d816e18229cde2b7304c4ee505cfd93b219
Vansh Thakor <163308114+Vansht1817@users.noreply.github.com>
2025-08-24 21:29:10 +0530

    Update p31.py

--- p31.py
+++ p31.py
@@ -1,19 +1,4 @@
 #Write a program to find the factorial of a given number using recursion.
-
-#def factorial(n):
-#    if n == 0 or n == 1:
-#        return 1
-#   else:
-#        return n * factorial(n - 1)
-
-
-# Input from user
-#number = int(input("Enter a number to find its factorial: "))
-# Calculate factorial
-#result = factorial(number)
-# Display result
-#print(f"The factorial of {number} is: {result}")
-
 def factorial(n):
     if n == 0 or n == 1:
         return 1
@@ -27,3 +12,4 @@ number = int(input("Enter a number to find its factorial: "))
 result = factorial(number)
 # Display result
 print(f"The factorial of {number} is: {result}")
+
