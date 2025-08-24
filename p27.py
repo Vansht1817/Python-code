ea9e0e44b5acbb0bf2c44da190db479fb5dcc78e
Vansh Thakor <163308114+Vansht1817@users.noreply.github.com>
2025-08-24 21:31:07 +0530

    Update p27.py

--- p27.py
+++ p27.py
@@ -1,11 +1,4 @@
 #Write a program to demonstrate the use of default arguments.
-#def stud_info(name,course="BCA"):
-#    print("name :",name)
-#    print("course :",course)
-
-#stud_info("pranjal")
-#stud_info("pranjal","bba")
-
 def display_info(name="Unknown", age=0):  
     print(f"Name: {name}")
     print(f"Age: {age}")
@@ -14,4 +7,5 @@ def display_info(name="Unknown", age=0):
 display_info()  
 display_info(name="Alice")    
 display_info(age=30)  
+
 display_info(name="Bob", age=25)  
